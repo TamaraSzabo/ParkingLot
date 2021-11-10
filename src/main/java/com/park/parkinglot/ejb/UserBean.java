c4afe5e5542eb72cd1ce9c5d153079a1cb4df870
DESKTOP-HHMF9L0\asus <tamara.szabo@ulbsibiu.ro>
2021-11-10 09:21:32 +0200

    Java Web App 6

---
+++ src/main/java/com/park/parkinglot/ejb/UserBean.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package com.park.parkinglot.ejb;
+
+import com.park.parkinglot.common.UserDetails;
+import com.park.parkinglot.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author asus
+ */
+@Stateless
+public class UserBean {
+
+    private static final Logger LOG = Logger.getLogger(UserBean.class.getName());
+    @PersistenceContext
+    private EntityManager em;
+    private Iterable<User> users;
+    
+   public List<UserDetails> getAllUsers() {
+         LOG.info("getAllUsers");
+       try {
+           Query query = em.createQuery("SELECT c FROM User c");
+            List<User> users = (List<User>) query.getResultList();
+            return copyUsersToDetails(users);
+        } catch (Exception ex) {
+            throw new EJBException(ex);
+        }
+    }
+   
+   private List<UserDetails> copyUsersToDetails(List<User> cars) {
+      List<UserDetails> detailsList = new ArrayList<>();
+      for (User user : users){
+          UserDetails userDetails = new UserDetails(user.getId(),user.getUsername(),user.getEmail(),user.getPosition());
+          detailsList.add(userDetails);
+      }
+      return detailsList;
+   }
+}
